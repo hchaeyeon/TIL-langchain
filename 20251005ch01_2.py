1f58e9968e33518947757ce7920912d0d3e5400a
chaeyeon <codus1733@gmail.com>
2025-10-05 16:45:39 +0900

    오픈 AI API 사용하여 메시지 보내고 응답받는 함수 생성

---
+++ 20251005ch01_2.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+from typing import List
+import os, sys
+
+load_dotenv()
+client = OpenAI()
+
+#요청에 사용할 프롬프트 템플릿 정의
+prompt_template = "주제 {topic}에 대해 짧은 설명을 해주세요."
+
+#메시지를 보내고 모델의 응답을 받는 함수
+def call_chat_model(messages: List[dict]):
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages,
+    )
+    return response.choices[0].message.content
+
+
+# 프롬프트 메시지 생성
+topic = "더블딥"
+messages = [
+    {"role": "user", "content": prompt_template.format(topic=topic)}
+]
+
+# 함수 호출 & 결과 저장
+answer = call_chat_model(messages)
+
+# 결과 출력
+print("GPT 응답:", answer)
---
+++ 20251005ch01_3.py
@@ -0,0 +1,25 @@
+
+#주어진 주제에 따라 설명을 요청하는 함수
+from openai import OpenAI
+from dotenv import load_dotenv
+from typing import List
+import os, sys
+
+prompt_template = "주제 {topic}에 대해 짧은 설명을 해주세요."
+load_dotenv()
+client = OpenAI()
+def call_chat_model(messages: List[dict]):
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages,
+    )
+    return response.choices[0].message.content
+
+def invoke_chain(topic : str):
+    prompt_value = prompt_template.format(topic = topic)
+    messages = [{"role" : "user", "content" : prompt_value}]
+    answer = call_chat_model(messages)
+    print(answer)
+    return answer
+    
+invoke_chain("LG트윈스")
