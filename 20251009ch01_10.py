680fdf6a87b9ac5d79f118b19cbebf607fd65967
chaeyeon <codus1733@gmail.com>
2025-10-10 16:46:56 +0900

    파이프 연산자 예제를 .pipe() 메서드로 구성한 예제

--- 20251009ch01_10.py
+++ 20251009ch01_10.py
@@ -3,7 +3,6 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-
 load_dotenv()
 
 model = ChatOpenAI(model= "gpt-4o-mini")
